551ccfadb7644461cdb52ee4bd350870ec016d3b
Ness <ness.sequoialabs.dev@gmail.com>
2025-03-05 10:18:45 -0600

    wip

--- sandbox/lab02/tests/unit/test_batches.py
+++ sandbox/lab02/tests/unit/test_batches.py
@@ -1,10 +1,9 @@
 from datetime import date
-from allocation.domain.model import Batch, OrderLine
+#from allocation.domain.model import Batch, OrderLine
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
-    line = OrderLine("order-ref", "SMALL-TABLE", 2)
+    pass
     
-    batch.allocate(line)
+
     
-    assert batch.available_quantity == 18
+
